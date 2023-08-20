4f2ebd9d8614d9ec232cb3c3c8432d1d0fb4c4a8
Muhammad Habib <habibsahabubit@gmail.com>
2023-08-21 03:31:28 +0500

    done

--- 2.Django-Apps-Views-URLs-Templates/core/urls.py
+++ 2.Django-Apps-Views-URLs-Templates/core/urls.py
@@ -20,5 +20,5 @@ from django.urls import path,include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("firstapp/",include("firstapp.url")),
-     path("secondapp/",include("secondapp.url")),
+    path("secondapp/",include("secondapp.url")),
 ]
